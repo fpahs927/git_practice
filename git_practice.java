395fbfb1b2f1300c0906bd5f81146e040177c962
[Github <[nami@i2max.co.kr]>
2023-04-07 13:00:08 +0900

    힘내

--- git_practice.java
+++ git_practice.java
@@ -1,5 +1,7 @@
 public class git_practice{
-public static void main(String args[]){
-    System.out.print("hello world"); 
+public static void main(String args[]){ 
+    for(int i=0; i<5; i++){
+        System.out.print("first name");  
+    }
     }
 }
