1bc418c0001074149338840b2ea84fe154f8b2a4
[Github <[nami@i2max.co.kr]>
2023-04-07 14:02:05 +0900

    이게 맞는가?

--- git_practice.java
+++ git_practice.java
@@ -1,7 +1,15 @@
 public class git_practice{
 public static void main(String args[]){ 
-    for(int i=0; i<5; i++){
-        System.out.print("first name");  
-    }
+    String s=" "; 
+    s= s.replace("zero", "0");
+    s =s.replace("one", "1"); //해당 문자열이 없으면 넘어간다  
+    s= s.replace("two", "2");
+    s= s.replace("three", "3");
+    s= s.replace("four", "4");
+    s= s.replace("five", "5");
+    s= s.replace("six", "6");
+    s= s.replace("seven", "7");
+    s= s.replace("eight", "8");
+    s= s.replace("nine", "9");
     }
 }
